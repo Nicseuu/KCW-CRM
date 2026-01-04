b5d97fc90126a7289ddf15c506b0bb6b2d9735ca
Nicseuu <jcabidoy147@gmail.com>
2026-01-05 02:08:17 +0800

    Update deps.py

--- app/api/deps.py
+++ app/api/deps.py
@@ -9,12 +9,11 @@ from app.core.db import SessionLocal, init_db
 
 def get_db() -> Generator[Session, None, None]:
     """
-    FastAPI dependency.
-    Initializes the DB session factory on first use.
+    FastAPI dependency that yields a SQLAlchemy session.
     """
     init_db()
-
     assert SessionLocal is not None  # for type checkers
+
     db: Session = SessionLocal()
     try:
         yield db
